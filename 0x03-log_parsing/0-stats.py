076bc6f519b51da1e8aeb78cf2ce93e43bfb3575
kerzers <ahra.ahlame@gmail.com>
2024-01-25 21:28:09 +0100

    fix sorted code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 """script that reads stdin line by line and computes metrics"""
+
 import sys
 
-
+code = {'200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
 count = 0
-size = 0
-status_code = ["200", "301", "400", "401", "403", "404", "405", "500"]
-code = {"200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
     for line in sys.stdin:
-        line_args = line.split(" ")
-        if len(line_args) > 4:
-            size += eval(line_args[-1])
-            if line_args[-2] in status_code:
-                code[line_args[-2]] += 1
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            size = int(line_list[-1])
+            if status_code in code.keys():
+                code[status_code] += 1
+            total_size += size
             count += 1
 
         if count == 10:
-            print("File size: {}".format(size))
-            for key, values in code.items():
-                if values != 0:
-                    print("{}: {}".format(key, values))
             count = 0
-            code = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-except KeyboardInterrupt:
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(code.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
     pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(code.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
