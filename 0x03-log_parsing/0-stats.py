f787989e2b975c4bae048ae57e95e1965979fd0d
kerzers <ahra.ahlame@gmail.com>
2024-01-25 21:30:57 +0100

    fix sorted code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+'''a script that reads stdin line by line and computes metrics'''
+
 
 import sys
 
-code = {'200': 0, '301': 0, '400': 0, '401': 0,
-        '403': 0, '404': 0, '405': 0, '500': 0}
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-count = 0
+counter = 0
 
 try:
     for line in sys.stdin:
         line_list = line.split(" ")
         if len(line_list) > 4:
-            status_code = line_list[-2]
+            code = line_list[-2]
             size = int(line_list[-1])
-            if status_code in code.keys():
-                code[status_code] += 1
+            if code in cache.keys():
+                cache[code] += 1
             total_size += size
-            count += 1
+            counter += 1
 
-        if count == 10:
-            count = 0
+        if counter == 10:
+            counter = 0
             print('File size: {}'.format(total_size))
-            for key, value in sorted(code.items()):
+            for key, value in sorted(cache.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
@@ -31,6 +32,6 @@ except Exception as err:
 
 finally:
     print('File size: {}'.format(total_size))
-    for key, value in sorted(code.items()):
+    for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
